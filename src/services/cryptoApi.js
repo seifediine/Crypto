3c1080a7537487551fd3cbeb40f405c4eb18d161
seifediine <seifeddine.elkorichi@hotmail.com>
2021-09-19 17:29:27 +0100

    layout

---
+++ src/services/cryptoApi.js
@@ -0,0 +1,22 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+
+const cryptoApiHeaders = {
+  'x-rapidapi-host': 'coinranking1.p.rapidapi.com',
+  'x-rapidapi-key': 'ff0fb4cbcemsh011afff03ce18fep12520djsncc2141137d41',
+}
+
+const baseUrl = 'https://coinranking1.p.rapidapi.com'
+
+const createRequest = (url) => ({ url, headers: cryptoApiHeaders })
+
+export const cryptoApi = createApi({
+  reducerPath: 'cryptoApi',
+  baseQuery: fetchBaseQuery({ baseUrl }),
+  endpoints: (builder) => ({
+    getCryptos: builder.query({
+      query: () => createRequest('/coins'),
+    }),
+  }),
+})
+
+export const { useGetCryptosQuery } = cryptoApi
